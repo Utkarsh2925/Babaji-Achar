00052b832bef835736fea775dbb140d7ed076c6c
Vaishnavi Kesarwani <kesarwanivaishnavi194@gmail.com>
2026-02-22 09:02:08 +0530

    fix(vercel): force pure static export to resolve 404

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    output: 'export',
+    images: { unoptimized: true },
     eslint: { ignoreDuringBuilds: true },
     typescript: { ignoreBuildErrors: true },
     trailingSlash: true,
